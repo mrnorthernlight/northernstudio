9edb3c7ba3c90aab4bf1797a3480adfb7a788741
Northern Studio <info@northernstudio.be>
2026-03-11 14:09:12 +0100

    Make homepage locale-aware

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -14,46 +13,14 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Northern Studio",
-  description: "Premium custom websites for professionals and businesses",
+  description: "PC builds, IT help, hardwareadvies en premium webdesign.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div
-          id="google_translate_element"
-          style={{
-            position: "fixed",
-            bottom: "20px",
-            right: "20px",
-            zIndex: 9999,
-            background: "white",
-            padding: "6px 10px",
-            borderRadius: "6px",
-            fontSize: "12px",
-          }}
-        />
-
+    <html lang="nl">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-
-        <Script id="google-translate-init" strategy="afterInteractive">
-          {`
-            function googleTranslateElementInit() {
-              new google.translate.TranslateElement(
-                { pageLanguage: 'en' },
-                'google_translate_element'
-              );
-            }
-          `}
-        </Script>
-
-        <Script
-          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
